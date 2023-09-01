ec48bc8b21bd6b576b42792d752845fa607d9cf9
Benjamin Gearhart <benjamingearhart@gmail.com>
2023-09-01 12:57:32 -0600

    index.php js function now redirecting to correctly selected page

--- index.php
+++ index.php
@@ -1,50 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
-  <link rel="stylesheet" href="style.css" />
-  <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
-  <script src="script.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"
+        integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
 </head>
-
 <body>
-  <div class="login">
-    <div class="login-header">
-      <img src="pcnm.png" />
+    <div class="login">
+        <div class="login-header">
+            <img src="pcnm.png" />
+        </div>
+        <form class="login-form" name="loginbox" action="" method="post" onsubmit="return setAction()">
+            <div class="wrapper">
+                <button class="learn-more" type="button" onclick="setAction('repair')">
+                    <span class="circle" aria-hidden="true">
+                        <span class="icon arrow"></span>
+                    </span>
+                    <span class="push button-text">Repair Tracker</span>
+                </button>
+                <button class="learn-more" type="button" onclick="setAction('time')">
+                    <span class="circle" aria-hidden="true">
+                        <span class="icon arrow"></span>
+                    </span>
+                    <span class="button-text">Time Clock</span>
+                </button>
+            </div>
+            <br />
+
+            <!-- Hidden input fields for values -->
+            <input type="hidden" name="button" id="buttonValue" value="">
+            <input type="text" name="name" id="nameInput" placeholder="Username" /><br />
+            <input type="password" name="password" id="passwordInput" placeholder="Password" />
+            <br />
+            <input type="hidden" name="RURI" value="<?php echo $ruri; ?>" />
+            <input type="hidden" name="METHOD" value="<?php echo $method; ?>" /><br />
+            <button class="learn-more" type="submit" id="submitButton">
+                <span class="circle" aria-hidden="true">
+                    <span class="icon arrow"></span>
+                </span>
+                <span class="button-text">Submit</span>
+            </button>
+        </form>
+
+        <script>
+            function setAction(option) {
+                var form = document.forms['loginbox'];
+                var buttonValueInput = form.querySelector('#buttonValue');
+                var nameInput = form.querySelector('#nameInput');
+                var passwordInput = form.querySelector('#passwordInput');
+                
+                // Get the selected option based on the button clicked
+                var selectedOption = option;
+                
+                buttonValueInput.value = selectedOption;
+
+                // You can access the input values here if needed
+                var username = nameInput.value;
+                var password = passwordInput.value;
+
+                if (selectedOption === 'repair') {
+                    form.action = 'rt14/repair/login.php';
+                } else if (selectedOption === 'time') {
+                    form.action = 'rt14/timeclock/login.php';
+                }
+
+                return true; // Allow the form to submit
+            }
+        </script>
     </div>
-    <form class="login-form" name="loginbox" action="" method="post">
-      <div class="wrapper">
-        <button class="learn-more" name="repair">
-          <span class="circle" aria-hidden="true">
-            <span class="icon arrow"></span>
-          </span>
-          <span class="push button-text">Repair Tracker</span>
-        </button>
-        <button class="learn-more" name="time">
-          <span class="circle" aria-hidden="true">
-            <span class="icon arrow"></span>
-          </span>
-          <span class="button-text">Time Clock</span>
-        </button>
-      </div>
-      <br />
-
-      <input type="text" name="name" placeholder="Username" /><br />
-      <input type="password" name="password" placeholder="Password" />
-      <br />
-      <input type="hidden" name="RURI" value="<?php echo $ruri; ?>" />
-      <input type="hidden" name="METHOD" value="<?php echo $method; ?>" /><br />
-      <button class="learn-more">
-        <span class="circle" aria-hidden="true">
-          <span class="icon arrow"></span>
-        </span>
-        <span class="button-text">Submit</span>
-      </button>
-    </form>
-  </div>
 </body>
-
 </html>
+
